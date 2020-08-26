284defcb45406933382563eefa3967449b70bdda
themudo <gethemudo@gmail.com>
2020-08-26 11:57:32 +0100

    Adaptation to ptwiki

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ from datetime import datetime
 import pandas as pd
 
 
-class SeroBOT(Bot):
+class WolfgangBOT(Bot):
 
-    """BOT que revierte desde ORES"""
+    """BOT que reverte a partir do ORES"""
 
     def __init__(self, generator, site=None, **kwargs):
         self.availableOptions.update({
             'gf': 0.085,
             'dm': 0.970,
-            'wiki': 'eswiki',
+            'wiki': 'ptwiki',
             'debug': False
         })
         """Constructor."""
-        super(SeroBOT, self).__init__(**kwargs)
+        super(WolfgangBOT, self).__init__(**kwargs)
         self.generator = generator
         self.site = site
         if self.site.logged_in() == False:
@@ -32,50 +32,50 @@ class SeroBOT(Bot):
     def run(self):
         for page in filter(lambda x: self.valid(x), self.generator):
             try:
-                revision, buena_fe, danina, resultado = self.checkORES(page)
+                revisao, boa_fe, danosa, resultado = self.checkORES(page)
             except:
                 continue
-            data = [revision, buena_fe, danina, resultado,
+            data = [revisao, boa_fe, danosa, resultado,
                     page._rcinfo.get('user'), page.title(), datetime.utcnow().strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())]
             if self.getOption('debug'):
                 print(data)
             self.do_log(data)
             self.do_reverse(page) if resultado == True else False
-            if (self.site.family.name == 'wikipedia' and self.site.lang == 'es'):
+            if (self.site.family.name == 'wikipedia' and self.site.lang == 'pt'):
                 self.check_user(page._rcinfo.get('user'),
                                 page.title()) if resultado == True else False
                 self.check_pagina(page.title()) if resultado == True else False
 
     def valid(self, page):
         """
-        Determina si un cambio es válido para poder invocar el endpoint de ORES
+        Determina se uma mudança é válida para poder invocar o endpoint do ORES
         Selecciona
-        * solo las ediciones,
-        * el usuario no es bot
-        * espacios de nombres 0 y 104 (principal y anexo)
-        * el usuario no soy yo mismo (SeroBOT)
+        * só as edições,
+        * o usuario não é bot
+        * domínio 0 (principal)
+        * o usuário não sou eu mesmo (WolfgangBOT)
 
-        @param page: página a chequear
+        @param page: página a verificar
         @return true
         """
-        return page._rcinfo.get('type') == 'edit' and page._rcinfo.get('bot') == False and page._rcinfo.get('namespace') in [0, 104] and page._rcinfo.get('user') != self.site.username()
+        return page._rcinfo.get('type') == 'edit' and page._rcinfo.get('bot') == False and page._rcinfo.get('namespace') in [0] and page._rcinfo.get('user') != self.site.username()
 
     def checkORES(self, page):
         headers = {
-            'User-Agent': 'SeroBOT - an ORES counter vandalism tool'
+            'User-Agent': 'WolfgangBOT - an ORES counter vandalism tool'
         }
         wiki = self.getOption('wiki')
-        revision = page._rcinfo.get('revision')
-        ores = str(revision.get('new'))
+        revisao = page._rcinfo.get('revisao')
+        ores = str(revisao.get('new'))
         url = 'https://ores.wikimedia.org/v3/scores/{0}/{1}'.format(wiki, ores)
         r = requests.get(url=url, headers=headers)
         data = r.json()
         try:
             goodfaith = data.get(wiki).get('scores').get(ores).get('goodfaith')
             damaging = data.get(wiki).get('scores').get(ores).get('damaging')
-            buena_fe = goodfaith.get('score').get('probability').get('true')
-            danina = damaging.get('score').get('probability').get('true')
-            return (ores, buena_fe, danina, True if buena_fe < self.getOption('gf') or danina > self.getOption('dm') else False)
+            boa_fe = goodfaith.get('score').get('probability').get('true')
+            danosa = damaging.get('score').get('probability').get('true')
+            return (ores, boa_fe, danosa, True if boa_fe < self.getOption('gf') or danosa > self.getOption('dm') else False)
         except:
             pywikibot.exception()
 
@@ -84,11 +84,11 @@ class SeroBOT(Bot):
             os.path.realpath(__file__)), self.getOption('wiki'))
         positivo = "{0}/log/{1}-positivo.log".format(os.path.dirname(
             os.path.realpath(__file__)), self.getOption('wiki'))
-        with open(general, encoding='utf-8', mode='a+') as archivo:
-            archivo.write(u'\t'.join(map(lambda x: str(x), data)) + u'\n')
+        with open(general, encoding='utf-8', mode='a+') as arquivo:
+            arquivo.write(u'\t'.join(map(lambda x: str(x), data)) + u'\n')
         if data[3] == True:
-            with open(positivo, encoding='utf-8', mode='a+') as archivo:
-                archivo.write(u'\t'.join(map(lambda x: str(x), data)) + u'\n')
+            with open(positivo, encoding='utf-8', mode='a+') as arquivo:
+                arquivo.write(u'\t'.join(map(lambda x: str(x), data)) + u'\n')
 
     def check_user(self, usuario, pagina):
         positivo = "{0}/log/{1}-positivo.log".format(os.path.dirname(
@@ -104,33 +104,33 @@ class SeroBOT(Bot):
             if self.getOption('debug'):
                 print('Avisando a ', usuario)
             talk = pywikibot.Page(self.site, title=usuario, ns=3)
-            talk.text += u"\n{{sust:Aviso prueba2|" + pagina + "}} ~~~~"
+            talk.text += u"\n{{subst:Av-teste|2|" + pagina + "}} ~~~~"
             talk.save(
-                comment=u'Aviso de pruebas a usuario tras reversiones consecutivas')
-            with open(os.path.dirname(os.path.realpath(__file__)) + '/log/discusiones.log', 'a+') as archivo:
-                archivo.write('\t'.join(map(lambda x: str(x), [usuario, pagina, datetime.utcnow(
+                comment=u'Aviso sobre testes a usuário depois de reversões consecutivas')
+            with open(os.path.dirname(os.path.realpath(__file__)) + '/log/discusiones.log', 'a+') as arquivo:
+                arquivo.write('\t'.join(map(lambda x: str(x), [usuario, pagina, datetime.utcnow(
                 ).strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())])) + '\n')
             return
         rows = df_reversas[user & past]
         if (len(rows) == 4):
             if self.getOption('debug'):
                 print('VEC a ', usuario)
-            with open(os.path.dirname(os.path.realpath(__file__)) + '/log/vec.log', 'a+') as archivo:
-                archivo.write('\t'.join(map(lambda x: str(x), [usuario, datetime.utcnow(
+            with open(os.path.dirname(os.path.realpath(__file__)) + '/log/vec.log', 'a+') as arquivo:
+                arquivo.write('\t'.join(map(lambda x: str(x), [usuario, datetime.utcnow(
                 ).strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())])) + '\n')
 
-            vec = pywikibot.Page(self.site, title='Vandalismo en curso', ns=4)
+            vec = pywikibot.Page(self.site, title='Vandalismo em curso', ns=4)
             tpl = u'{{subst:'
-            tpl += 'ReportevandalismoIP' if User.isAnonymous() else 'Reportevandalismo'
+            tpl += 'bloquear'
             tpl += u'|1='+usuario
-            tpl += u'|2=Reversiones: ' + \
+            tpl += u'|2=Reversões: ' + \
                 (', '.join(
                     map(lambda x: u'[[Special:Diff/'+str(x)+'|diff: '+str(x)+']]', rows[0])))
             tpl += u'}}'
             vec.text += "\n"+tpl
             try:
-                vec.save(comment=u'Reportando al usuario [[Special:Contributions/' +
-                         usuario+'|'+usuario+']] por posibles reversiones vandálicas')
+                vec.save(comment=u'Reportando o usuário [[Special:Contributions/' +
+                         usuario+'|'+usuario+']] por possível vandalismo')
             except:
                 pass
         return
@@ -148,25 +148,25 @@ class SeroBOT(Bot):
         if (len(rows) < 6 or users < 2):
             return
         if self.getOption('debug'):
-            print('Solicitud de protección de página ', pagina)
+            print('Pedido de proteção de página ', pagina)
 
         tabp = pywikibot.Page(
-            self.site, title='Tablón de anuncios de los bibliotecarios/Portal/Archivo/Protección de artículos/Actual', ns=4)
+            self.site, title='Pedidos/Proteção', ns=4)
         if (tabp.get().find('{{{{a|{0}}}}}'.format(pagina)) != -1):
             return
-        tpl = '{{{{subst:Usuario:SeroBOT/TABP|pagina={0}|firma=~~~~}}}}'.format(
+        tpl = '{{{{subst:Proteger|pagina={0}|razão=Reversões consecutivas pelo WolfgangBOT. ~~~~}}}}'.format(
             pagina)
         tabp.text += "\n"+tpl
         try:
             tabp.save(
-                comment=u'Solicitando protección de {0} por reversiones consecutivas'.format(pagina))
+                comment=u'Pedindo proteção de {0} por reversões consecutivas'.format(pagina))
         except:
             pass
         return
 
     def do_reverse(self, page):
         if self.getOption('debug'):
-            print('>> Reversion', page._rcinfo.get('user'), page.title())
+            print('>> Reversão', page._rcinfo.get('user'), page.title())
         try:
             token = pywikibot.data.api.Request(site=self.site, parameters={
                                                'action': 'query', 'meta': 'tokens', 'type': 'rollback'}).submit()['query']['tokens']['rollbacktoken']
@@ -181,12 +181,12 @@ class SeroBOT(Bot):
                 site=self.site, parameters=parameters).submit()
             return
         except Exception as e:
-            print(u'No puedo revertir la página ', e)
+            print(u'Não consigo reverter a página ', e)
 
 
 def main(*args):
     """
-    Procesa los parámetros desde la línea de comandos
+    Processa os parâmetros desde a linha de comandos
 
     If args is an empty list, sys.argv is used.
 
@@ -206,12 +206,12 @@ def main(*args):
             opts['debug'] = True
 
     site = pywikibot.Site()
-    if 'wiki' in opts and opts['wiki'] != 'eswiki':
+    if 'wiki' in opts and opts['wiki'] != 'ptwiki':
         lang = opts['wiki'][0:2]
         family = opts['wiki'][2:]
         site = pywikibot.Site(lang, family)
 
-    bot = SeroBOT(pagegenerators.LiveRCPageGenerator(site),
+    bot = WolfgangBOT(pagegenerators.LiveRCPageGenerator(site),
                   site=site, **opts)
     bot.run()
 
